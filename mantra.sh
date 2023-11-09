8880c0dbcd8db0f9cebca96134a786dd4a0d1c9a
Bang Pateng <38981255+bangpateng@users.noreply.github.com>
2023-11-09 12:26:30 +0700

    Update mantra.sh
    Update Chain

--- mantra.sh
+++ mantra.sh
@@ -28,13 +28,15 @@ fi
 if [ ! $WALLET ]; then
 	echo "export WALLET=wallet" >> $HOME/.bash_profile
 fi
-echo "export MANTRA_CHAIN_ID=mantrachain-1" >> $HOME/.bash_profile
+echo "export MANTRA_CHAIN_ID=mantrachain-testnet-1" >> $HOME/.bash_profile
+echo "export MANTRA_PORT=22" >> $HOME/.bash_profile
 source $HOME/.bash_profile
 
 echo '================================================='
 echo -e "Your node name: \e[1m\e[32m$NODENAME\e[0m"
 echo -e "Your wallet name: \e[1m\e[32m$WALLET\e[0m"
 echo -e "Your chain name: \e[1m\e[32m$MANTRA_CHAIN_ID\e[0m"
+echo -e "Your port name: \e[1m\e[32m$MANTRA_PORT\e[0m"
 echo '================================================='
 sleep 2
 
@@ -63,9 +65,10 @@ sudo wget -P /usr/lib https://github.com/CosmWasm/wasmvm/releases/download/v1.3.
 sudo mv ./mantrachaind /usr/local/bin
 
 # config
-mantrachanid config chain-id $MANTRA_CHAIN_ID
+mantrachaind config node tcp://localhost:${MANTRA_PORT}657
+mantrachaind config keyring-backend os
+mantrachaind config chain-id mantrachain-testnet-1
 mantrachaind init $NODENAME --chain-id $MANTRA_CHAIN_ID
-mantrachaind config keyring-backend test
 
 # download genesis
 wget -O $HOME/.mantrachain/config/genesis.json https://testnet-files.itrocket.net/mantra/genesis.json
@@ -76,15 +79,32 @@ SEEDS="a9a71700397ce950a9396421877196ac19e7cde0@mantra-testnet-seed.itrocket.net
 PEERS="1a46b1db53d1ff3dbec56ec93269f6a0d15faeb4@mantra-testnet-peer.itrocket.net:22656,63763bfb78d296187754c367a9740e24730a7fc4@167.235.14.83:32656,64691a4202c1ad29a416b21ce21bfc9659783406@34.136.169.18:26656,d44eb6a1ea69263eb0a61bab354fb267396b27e1@34.70.189.2:26656,62cadc3da28e1a4785a2abf76c40f1c4e0eaeebd@34.123.40.240:26656,c4bec34390d2ab1004b9a25580c75e4743e033a1@65.108.72.253:22656,e6921a8a228e12ebab0ab70d9bcdb5364c5dece5@65.108.200.40:47656,2d2f8b62feee6b0fcbdec78d51d4ba9959e33c87@65.108.124.219:34656,4a22a9cbabe4313674d2058a964aef2863af9213@185.197.251.195:26656,c0828205f0dea4ef6feb61ee7a9e8f376be210f4@161.97.149.123:29656,30235fa097d100a14d2b534fdbf67e34e8d5f6cf@65.21.133.86:21656"
 sed -i -e "s/^seeds *=.*/seeds = \"$SEEDS\"/; s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $HOME/.mantrachain/config/config.toml
 
+# set custom ports in app.toml
+sed -i.bak -e "s%:1317%:${MANTRA_PORT}317%g;
+s%:8080%:${MANTRA_PORT}080%g;
+s%:9090%:${MANTRA_PORT}090%g;
+s%:9091%:${MANTRA_PORT}091%g;
+s%:8545%:${MANTRA_PORT}545%g;
+s%:8546%:${MANTRA_PORT}546%g;
+s%:6065%:${MANTRA_PORT}065%g" $HOME/.mantrachain/config/app.toml
+
+# set custom ports in config.toml file
+sed -i.bak -e "s%:26658%:${MANTRA_PORT}658%g;
+s%:26657%:${MANTRA_PORT}657%g;
+s%:6060%:${MANTRA_PORT}060%g;
+s%:26656%:${MANTRA_PORT}656%g;
+s%^external_address = \"\"%external_address = \"$(wget -qO- eth0.me):${MANTRA_PORT}656\"%;
+s%:26660%:${MANTRA_PORT}660%g" $HOME/.mantrachain/config/config.toml
+
 # config pruning
-pruning="custom"
-pruning_keep_recent="100"
-pruning_keep_every="0"
-pruning_interval="10"
-sed -i -e "s/^pruning *=.*/pruning = \"$pruning\"/" $HOME/.mantrachain/config/app.toml
-sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"$pruning_keep_recent\"/" $HOME/.mantrachain/config/app.toml
-sed -i -e "s/^pruning-keep-every *=.*/pruning-keep-every = \"$pruning_keep_every\"/" $HOME/.mantrachain/config/app.toml
-sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"$pruning_interval\"/" $HOME/.mantrachain/config/app.toml
+sed -i -e "s/^pruning *=.*/pruning = \"nothing\"/" $HOME/.mantrachain/config/app.toml
+sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"100\"/" $HOME/.mantrachain/config/app.toml
+sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"50\"/" $HOME/.mantrachain/config/app.toml
+
+# set minimum gas price, enable prometheus and disable indexing
+sed -i 's/minimum-gas-prices =.*/minimum-gas-prices = "0.0001uaum"/g' $HOME/.mantrachain/config/app.toml
+sed -i -e "s/prometheus = false/prometheus = true/" $HOME/.mantrachain/config/config.toml
+sed -i -e "s/^indexer *=.*/indexer = \"null\"/" $HOME/.mantrachain/config/config.toml
 
 echo -e "\e[1m\e[32m4. Starting service... \e[0m" && sleep 1
 # create service
@@ -97,17 +117,25 @@ After=network-online.target
 User=$USER
 ExecStart=$(which mantrachaind) start --home $HOME/.mantrachain
 Restart=on-failure
-RestartSec=3
+RestartSec=5
 LimitNOFILE=65535
 
 [Install]
 WantedBy=multi-user.target
 EOF
 
-# start service
+echo -e "\e[1m\e[32m5. reset and snapshoot \e[0m" && sleep 1
+# reset and download snapshot
+mantrachaind tendermint unsafe-reset-all --home $HOME/.mantrachain
+if curl -s --head curl https://testnet-files.itrocket.net/mantra/snap_mantra.tar.lz4 | head -n 1 | grep "200" > /dev/null; then
+  curl https://testnet-files.itrocket.net/mantra/snap_mantra.tar.lz4 | lz4 -dc - | tar -xf - -C $HOME/.mantrachain
+    else
+  echo no have snap
+fi
+
 sudo systemctl daemon-reload
 sudo systemctl enable mantrachaind
-sudo systemctl start mantrachaind
+sudo systemctl restart mantrachaind && sudo journalctl -u mantrachaind -f
 
 echo '=============== SETUP FINISHED BANG ==================='
 echo -e 'To check logs: \e[1m\e[32mjournalctl -fu mantrachaind -o cat\e[0m'
